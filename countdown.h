f81e7657215536b1940f98803dd41fe518f7c316
Ciaron Rider <ciaron@ridernator.com>
2023-10-22 22:11:54 +0100

    Commented code

--- countdown.h
+++ countdown.h
@@ -4,14 +4,33 @@
 #include <vector>
 #include <string>
 
+/**
+ * Class which solves the Countdown maths questions
+ **/
 class Countdown{
     public:
+        /**
+         * Constructor for this class
+         * 
+         * @param numbers The input numbers (As many as you like)
+         * @param target The target to hit
+         **/
         Countdown(const std::vector<int64_t>& numbers,
                   const int64_t target);
 
     private:
+        /**
+         * Recursive method to solve the problem
+         * 
+         * @param numbers The current collection of numbers
+         * @param target The target to hit
+         * @return Returns true if solved of false if this path has no solution
+         **/
         bool iterate(const std::vector<int64_t>& numbers,
                      const int64_t target);
 
+        /**
+         * Place to store result steps
+         **/
         std::vector<std::string> results;
 };
